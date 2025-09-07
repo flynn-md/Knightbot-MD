4cd039cefd5fd663c58182129f9119475c764c3a
flynn-md <chrspskariuki@gmail.com>
2025-09-08 02:35:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,8 @@ const settings = {
   ownerNumber: '254753817025', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  maxStoreMessages: 20, 
-  storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.1.6",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  version: "2.0.6",
 };
 
 module.exports = settings;
